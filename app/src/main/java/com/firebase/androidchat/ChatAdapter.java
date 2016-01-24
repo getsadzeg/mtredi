59c9554d12809ac409b81920b72b105d735cdb0e
getsadzeg <getsadzeguram@gmail.com>
2016-01-24 13:39:17 +0400

    setting message statuses

--- app/src/main/java/com/firebase/androidchat/ChatAdapter.java
+++ app/src/main/java/com/firebase/androidchat/ChatAdapter.java
@@ -1,10 +1,12 @@
 package com.firebase.androidchat;
 
 
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class ChatAdapter extends BaseAdapter {
     LayoutInflater layoutInflater;
@@ -27,6 +29,26 @@ public class ChatAdapter extends BaseAdapter {
             v = layoutInflater.inflate(R.layout.chat_item_sent, null);
         else
             v = layoutInflater.inflate(R.layout.chat_item_rcv, null);
+        TextView label = (TextView) v.findViewById(R.id.lbl1);
+        label.setText(DateUtils.getRelativeDateTimeString(this, conv //not compiling yet because of 'this'
+                        .getDate().getTime(), DateUtils.SECOND_IN_MILLIS,
+                DateUtils.DAY_IN_MILLIS, 0));
+        TextView textlbl = (TextView) v.findViewById(R.id.lbl2);
+        textlbl.setText(conv.getMessage());
+        TextView statuslbl = (TextView) v.findViewById(R.id.lbl3);
+        if(conv.isSent()) {
+            if(conv.getStatus() == Conversation.STATUS_SENDING)
+                statuslbl.setText("იგზავნება..");
+            if(conv.getStatus() == Conversation.STATUS_SENT)
+                statuslbl.setText("გაიგზავნა");
+            if(conv.getStatus() == Conversation.STATUS_SEEN)
+                statuslbl.setText("ნანახია"); //Here we go, probably.
+            else statuslbl.setText("არ გაიგზავნა");
+        }
+        else { // go to isSent implementation. if isSent is false, so message is received, not sent. So look at code:
+            statuslbl.setText(""); //we set no text on receiver's chat on received message
+            conv.setStatus(Conversation.STATUS_SEEN); //we set STATUS_SEEN
+        }
 
         return v;
     }
